da65a940efa12f03f8b244174f14e93324e0dbd3
fugbot <18075133+fugbot@users.noreply.github.com>
2024-03-18 10:27:39 +0000

    Add basic calculator operation

---
+++ calculator.js
@@ -0,0 +1,38 @@
+let firstNum;
+let operator;
+let secondNum;
+
+function operate(operator, num1, num2){
+    if(operator === "add"){
+        add(num1, num2);
+    } else if(operator == "subtract"){
+        subtract(num1, num2);
+    } else if(operator == "multiply"){
+        multiply(num1, num2);
+    } else if(operator == "divide"){
+        divide(num1, num2);
+    }
+
+}
+
+function add(a,b){
+    console.log(a + b);
+}
+
+function subtract(a,b){
+    console.log(a - b);
+}
+
+function multiply(a,b){
+    console.log(a * b);
+}
+
+function divide(a,b){
+    console.log(a / b);
+}
+
+add(0, 5);
+subtract(2,3);
+multiply(2, 4);
+divide(10,2);
+operate("add", 5, 3);
